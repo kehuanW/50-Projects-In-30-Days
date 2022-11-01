99a2555ca028a434ef8c7fe1e2126c50cb6de733
Kehuan <kehuanwang123@gmail.com>
2022-11-01 23:53:45 +1030

    9th Proj: add js

--- 09_Sound_Board/script.js
+++ 09_Sound_Board/script.js
@@ -4,5 +4,18 @@ audio_ids.forEach((audio_id) => {
     const btn = document.createElement('button')
     btn.classList.add('btn')
     btn.innerText = audio_id
+
+    btn.addEventListener('click', () => {
+        stopAllSongs()
+        document.getElementsById(audio_id).play()
+    })
     document.getElementById('buttons').appendChild(btn)
 })
+
+function stopAllSongs() {
+    audio_ids.forEach(audio_id => {
+        const song = document.getElementById(audio_id)
+        song.onpause();
+        song.currentTime = 0;
+    })
+}
